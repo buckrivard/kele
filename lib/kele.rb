47cf47416607931d876a56fefbd0aecfa1c20bb5
Buck Rivard <buckrivard@gmail.com>
2017-04-05 12:45:33 -0700

    #get_me

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,21 @@
 require 'httparty'
 require 'pry'
+require 'json'
 
 class Kele
 	include HTTParty
 	BASE_URI = 'https://www.bloc.io/api/v1'
+
 	def initialize(email, password)
 		@email = email
 		@password = password
 		retrieve_auth_token
+	end
+
+	def get_me
+		retrieve_auth_token
+		response = self.class.get(BASE_URI + '/users/me', headers: {"authorization" => @auth_token })
+		JSON.parse(response.body)
 	end
 
 	private
