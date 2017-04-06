c04f7ade7e3d1c3d9fbfd2dc7652bed091673608
Buck Rivard <buckrivard@gmail.com>
2017-04-05 17:14:01 -0700

    bug crash diagnostics

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,6 +17,13 @@ class Kele
 		JSON.parse(response.body)
 	end
 
+	def get_mentor_availability(mentor_id)
+		retrieve_auth_token
+		response = self.class.get(BASE_URI + "/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token })
+		JSON.parse(response.body, :array_class => true)
+		binding.pry
+	end
+
 	private
 
 	def retrieve_auth_token
